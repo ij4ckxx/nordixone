ea9b3bbdbfdd4e2c77596d46409b420145dd4bc1
Jegan <mpjegannathanmech@gmail.com>
2025-10-15 23:31:25 +0530

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex h-screen bg-black text-white text-7xl font-bold'>
+      New project
+    </div>
+  );
+}
+
+export default App;

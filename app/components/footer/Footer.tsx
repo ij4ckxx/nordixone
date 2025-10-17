d5be207a1ca0a40908df9f4df7dfff4168cf58e8
jegan-ges <jmp@ges.com>
2025-10-17 22:07:37 +0530

    footer completed

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -48,27 +48,31 @@ const Footer = () => {
     <footer className="font-poppins p-[60px_5vw] bg-black text-white">
       <div className="flex flex-col gap-10">
         {/* first */}
-        <div className="flex flex-row items-start ">
-          <div className="flex flex-col gap-5 justify-between basis-1/3">
+        <div className="flex flex-col gap-8 md:flex-row md:items-start md:gap-2">
+          <div className="flex flex-col gap-5 justify-between basis-full md:basis-1/3">
             <Image
               src={nordixoneLogo}
               width={100}
               height={65}
               alt="nordixone-logo"
             />
-            <div className="w-[80%]">
+            <div className="w-full md:w-[95%] lg:w-[80%]">
               We help TRANSFORM organization's operations through Enterprise
               Applications
             </div>
           </div>
-          <div className="basis-2/3 flex flex-row gap-[8rem]">
+          <div className="md:basis-2/3 basis-full flex flex-row justify-between md:justify-start md:gap-[5rem] lg:gap-[8rem] gap-5">
             <div className="flex flex-col gap-4">
               <div className="text-nor-primary uppercase text-[1.375rem] font-medium tracking-[0px]">
                 {companyLink?.title}
               </div>
               <div className="flex flex-col gap-1">
                 {companyLink?.children?.map((item) => (
-                  <Link href={item?.url} key={item?.label}>
+                  <Link
+                    className="hover:text-nor-primary duration-300 ease-in-out focus:text-nor-primary transition-colors"
+                    href={item?.url}
+                    key={item?.label}
+                  >
                     {item?.label}
                   </Link>
                 ))}
@@ -80,7 +84,11 @@ const Footer = () => {
               </div>
               <div className="flex flex-col gap-1">
                 {serviceLink?.children?.map((item) => (
-                  <Link href={item?.url} key={item?.label}>
+                  <Link
+                    className="hover:text-nor-primary duration-300 ease-in-out focus:text-nor-primary transition-colors"
+                    href={item?.url}
+                    key={item?.label}
+                  >
                     {item?.label}
                   </Link>
                 ))}
@@ -89,14 +97,18 @@ const Footer = () => {
           </div>
         </div>
         {/* second */}
-        <div className="flex flex-row items-start ">
-          <div className="flex flex-col gap-5 justify-between basis-1/3">
+        <div className="flex flex-col-reverse sm:flex-col md:flex-row md:items-start gap-8 md:gap-2">
+          <div className="flex flex-col gap-5  basis-full md:basis-1/3">
             <div className="flex flex-col w-max">
-              <div className="text-nor-primary uppercase text-[1.375rem] font-medium tracking-[0px]">
+              <div className="text-nor-primary uppercase text-[1.375rem] font-medium 1tracking-[1px]">
                 FOLLOW OUR WORK
               </div>
               <div className="flex flex-row items-center justify-between gap-1 mt-4 md:mt-5">
-                <div className="flex justify-center items-center size-[30px] rounded-full bg-white">
+                <Link
+                  href={"https://x.com/NordiXOne_Ind"}
+                  target="_blank"
+                  className="flex justify-center items-center size-[30px] rounded-full bg-white"
+                >
                   <Image
                     className="size-5"
                     alt="twitter"
@@ -104,43 +116,75 @@ const Footer = () => {
                     height={20}
                     src={twitterIcon}
                   />
-                </div>
-                <div className="flex justify-center items-center size-[30px] rounded-full bg-white">
+                </Link>
+                <Link
+                  href={"https://in.linkedin.com/company/nordixone"}
+                  target="_blank"
+                  className="flex justify-center items-center size-[30px] rounded-full bg-white"
+                >
                   <Linkedin className="stroke-black size-5 fill-black" />
-                </div>
-                <div className="flex justify-center items-center size-[30px] rounded-full bg-white">
+                </Link>
+                <Link
+                  href={"https://www.instagram.com/NordiXOne/"}
+                  className="flex justify-center items-center size-[30px] rounded-full bg-white"
+                >
                   <Instagram className="stroke-black size-5" />
-                </div>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="basis-2/3 flex flex-row gap-[8rem]">
-            <div className="flex flex-col gap-4">
+          <div className="md:basis-2/3 basis-full flex flex-row">
+            <div className="flex flex-col gap-4 w-full md:w-auto">
               <div className="text-nor-primary uppercase text-[1.375rem] font-medium tracking-[0px]">
                 OUR LOCATIONS
               </div>
-              <div className="flex flex-row gap-[8rem]">
-                <div className="flex flex-col gap-1">
-                  <div className="flex flex-row items-center gap-0.5">
-                    <MapPin className="text-nor-primary" />
+              <div className="flex flex-col gap-8  sm:flex-row sm:justify-start  sm:gap-[5rem] lg:gap-[8rem]">
+                <div className="flex flex-col gap-1.5">
+                  <div className="flex flex-row items-center gap-1 mb-0.5">
+                    <MapPin className="text-nor-primary size-[1rem] [&>circle]:text-black [&>circle]:fill-black [&>path]:bg-nor-primary  [&>path]:fill-nor-primary" />
                     <span>INDIA</span>
                   </div>
-                  <div>Chennai Business Office</div>
-                  <div>No: 764, Anna Salai,</div>
-                  <div>Nandanam,</div>
-                  <div>Chennai-600035,Tamilnadu.</div>
-                  <div className="flex flex-row items-center gap-0.5">
-                    <Phone className="fill-white stroke-black" />
+                  <div className="ml-4">Chennai Business Office</div>
+                  <div className="ml-4">No: 764, Anna Salai,</div>
+                  <div className="ml-4">Nandanam,</div>
+                  <div className="ml-4">Chennai-600035,Tamilnadu.</div>
+                  <div className="flex flex-row items-center gap-1 ml-4">
+                    <Phone className="fill-white stroke-black size-[1rem] scale-[-1] rotate-90 " />
                     <Link href="tel:+91 90878 69016">+91 90878 69016</Link>
                   </div>
-                  <div className="flex flex-row items-center gap-0.5">
-                    <Mail className=" bg-black" />
-                    <Link href={"mailto:info@nordixone.com"}>
+                  <div className="flex flex-row items-center gap-1">
+                    <Mail className=" bg-black size-[1rem]" />
+                    <Link
+                      className="text-[#FF6F0F] hover:text-[#FF6F0F]/50 duration-300 transition-colors ease-in-out"
+                      href={"mailto:info@nordixone.com"}
+                    >
                       info@nordixone.com
                     </Link>
                   </div>
                 </div>
-                <div></div>
+                <div className="flex flex-col gap-1.5">
+                  <div className="flex flex-row items-center gap-1 mb-0.5">
+                    <MapPin className="text-nor-primary size-[1rem] [&>circle]:text-black [&>circle]:fill-black [&>path]:bg-nor-primary  [&>path]:fill-nor-primary" />
+                    <span>INDIA</span>
+                  </div>
+                  <div className="ml-4">Chennai Regd Office</div>
+                  <div className="ml-4">No: 472, Anna Salai,</div>
+                  <div className="ml-4">Nandanam,</div>
+                  <div className="ml-4">Chennai-600035,Tamilnadu.</div>
+                  <div className="flex flex-row items-center gap-1 ml-4">
+                    <Phone className="fill-white stroke-black size-[1rem] scale-[-1] rotate-90 " />
+                    <Link href="tel:+91 98410 07060"> +91 98410 07060</Link>
+                  </div>
+                  <div className="flex flex-row items-center gap-1">
+                    <Mail className=" bg-black size-[1rem]" />
+                    <Link
+                      className="text-[#FF6F0F] hover:text-[#FF6F0F]/50 duration-300 transition-colors ease-in-out"
+                      href={"mailto:info@nordixone.com"}
+                    >
+                      info@nordixone.com
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -149,11 +193,5 @@ const Footer = () => {
     </footer>
   );
 };
-
-// <MapPin />
-// <Phone />
-{
-  /* <Mail /> */
-}
 
 export default Footer;
